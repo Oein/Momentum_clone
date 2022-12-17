670141c79da1feff8d43a91045ea982cbb824cd5
Oein <oein0219@gmail.com>
2022-12-17 19:11:53 +0900

    greeting js

---
+++ public/greeting.js
@@ -0,0 +1,14 @@
+let greetingElement = document.getElementById("greet");
+
+function getGreeting() {
+    let hour = (new Date()).getHours();
+    if (hour >= 12) return "evening"
+    else return "morning"
+}
+
+let greetInterval = () => {
+    greetingElement.innerText = `Good ${getGreeting()}, ${localStorage.getItem("user.name")}.`
+};
+
+setInterval(greetInterval, 1000);
+greetInterval();
