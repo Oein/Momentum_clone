e3d18b5ed3494c3a8300b0a702b6a5f909c594cb
Oein <oein0219@gmail.com>
2022-12-17 20:56:40 +0900

    move into function

--- public/greeting.js
+++ public/greeting.js
@@ -1,14 +1,18 @@
-let greetingElement = document.getElementById("greet");
+(function () {
+  let greetingElement = document.getElementById("greet");
 
-function getGreeting() {
-    let hour = (new Date()).getHours();
-    if (hour >= 12) return "evening"
-    else return "morning"
-}
+  function getGreeting() {
+    let hour = new Date().getHours();
+    if (hour >= 12) return "evening";
+    else return "morning";
+  }
 
-let greetInterval = () => {
-    greetingElement.innerText = `Good ${getGreeting()}, ${localStorage.getItem("user.name")}.`
-};
+  let greetInterval = () => {
+    greetingElement.innerText = `Good ${getGreeting()}, ${localStorage.getItem(
+      "user.name"
+    )}.`;
+  };
 
-setInterval(greetInterval, 1000);
-greetInterval();
+  setInterval(greetInterval, 1000);
+  greetInterval();
+})();
