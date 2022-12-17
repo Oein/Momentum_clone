7fd25e4493723eab661a1e5966c2b24cb26d86ea
Oein <oein0219@gmail.com>
2022-12-17 19:12:14 +0900

    time js

---
+++ public/time.js
@@ -0,0 +1,16 @@
+let timeElement = document.getElementById("time");
+
+function pad2(number) {
+    return (number < 10 ? '0' : '') + number;
+}
+
+let timeInterval = () => {
+    let now = new Date();
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+
+    time.innerText = `${pad2(hour)}:${pad2(minutes)}`;
+};
+
+setInterval(timeInterval, 1000);
+timeInterval();
