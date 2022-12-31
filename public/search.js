0aaf408ad8d8a3123ec49a30fcba0f8d656ce429
Oein <oein0219@gmail.com>
2022-12-31 10:02:33 +0900

    add search function to icon

--- public/search.js
+++ public/search.js
@@ -3,3 +3,8 @@ document.getElementById("search").addEventListener("keypress", (e) => {
     location.href = `https://www.google.com/search?q=${e.target.value}`;
   }
 });
+
+document.getElementById("searchIcon").addEventListener("click", (e) => {
+  let v = document.getElementById("search").value;
+  if (v && v.length > 0) location.href = `https://www.google.com/search?q=${v}`;
+});
